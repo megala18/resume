6ad17b7f82f044deaeef662b9c72c55a9549f724
megala18 <89910285+megala18@users.noreply.github.com>
2021-09-03 17:40:19 +0530

    my resume

---
+++ New folder/resume.js
@@ -0,0 +1,29 @@
+function personal(){
+     document.getElementById("per").style.display="block";
+     document.getElementById("pro").style.display="none";
+     document.getElementById("edu").style.display="none";
+     document.getElementById("skill").style.display="none";
+     document.getElementById("con").style.display="none"
+}
+function education(){
+    document.getElementById("edu").style.display="block";
+    document.getElementById("per").style.display="none";
+    document.getElementById("pro").style.display="none";
+    document.getElementById("con").style.display="none";
+    document.getElementById("skill").style.display="none"
+}
+function myskill(){
+    document.getElementById("skill").style.display="block";
+    document.getElementById("per").style.display="none";
+    document.getElementById("pro").style.display="none";
+    document.getElementById("edu").style.display="none";
+    document.getElementById("con").style.display="none"
+}
+function project(){
+    document.getElementById("pro").style.display="block";
+    document.getElementById("per").style.display="none";
+    document.getElementById("edu").style.display="none";
+    document.getElementById("skill").style.display="none";
+    document.getElementById("con").style.display="none"
+}
+
